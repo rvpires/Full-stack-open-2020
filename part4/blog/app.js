9f841505483eb817586bc9f54c2491ca8d4d673a
rvp <rodrigovazpires@hotmail.com>
2020-04-21 17:14:21 +0100

    added part3 exercises and completed exercises Exercises 4.1.-4.2

---
+++ part4/blog/app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoUrl = config.MONGODB_URI
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
+
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
