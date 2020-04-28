3e6fbf635d05db6c505d2791a5f6666e36f798de
rvp <rodrigovazpires@hotmail.com>
2020-04-28 21:33:35 +0100

    Exercise 5.7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,52 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  const [details, setDetails] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+
+  const toggleDetails = () => {
+    if (details === true) {
+      setDetails(false)
+      setButtonText('view')
+    }
+
+    else if (details === false) {
+      setDetails(true)
+      setButtonText('hide')
+    }
+  }
+
+  const showWhenVisible = { display: details ? '' : 'none' }
+
+
+  return (
+
+
+    <div style={blogStyle}>
+      
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleDetails}>{buttonText}</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        <p>{blog.url} </p>
+        <p>{blog.likes} <button>like</button></p>
+        <p>{blog.author}</p>
+      </div>
+
+    </div>
+
+  )
+}
 
 export default Blog
