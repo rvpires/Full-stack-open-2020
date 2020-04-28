63406f23c31157c7b1749d1dbcf57fe10c0f5f0d
rvp <rodrigovazpires@hotmail.com>
2020-04-28 20:00:01 +0100

    exercise 5.5 done

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,6 +3,10 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import AddForm from './components/AddForm'
+import Togglable from './components/Togglable'
+import LoginForm from './components/LoginForm'
+
 const App = () => {
 
   const [blogs, setBlogs] = useState([])
@@ -17,6 +21,7 @@ const App = () => {
   const [notificationMessage, setNotificationMessage] = useState(null)
   const [notificationStyle, setNotificationSyle] = useState('error')
 
+  const addFormRef = React.createRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -36,13 +41,12 @@ const App = () => {
   }, [])
 
 
-  const sendNotification = (text, style) =>
-  {
+  const sendNotification = (text, style) => {
     setNotificationSyle(style)
-      setNotificationMessage(text)
-      setTimeout(() => {
-        setNotificationMessage(null)
-      }, 5000)
+    setNotificationMessage(text)
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
 
   }
 
@@ -58,45 +62,44 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
-      
+
     }
     catch (exception) {
 
-      sendNotification('Credentials are wrong' , 'error')
+      sendNotification('Credentials are wrong', 'error')
     }
 
 
   }
 
   const createNewBlog = async (event) => {
-    
+
     event.preventDefault()
 
-    try
-    {
-      const newBlog = 
+    try {
+      const newBlog =
       {
-        'title' : title,
-        'author' : author,
-        'url' : url
+        'title': title,
+        'author': author,
+        'url': url
       }
+
+      addFormRef.current.toggleVisibility()
 
       const result = await blogService.create(newBlog)
       setTitle('')
       setAuthor('')
       setUrl('')
 
-      setBlogs(blogs.concat(result)) 
-      sendNotification(`New blog ${newBlog.title} by ${newBlog.author} was created successfuly.` , 'success')
+      setBlogs(blogs.concat(result))
+      sendNotification(`New blog ${newBlog.title} by ${newBlog.author} was created successfuly.`, 'success')
 
     }
-    catch(exception)
-    {
-      sendNotification('Could not add blog.' , 'error')
+    catch (exception) {
+      sendNotification('Could not add blog.', 'error')
     }
 
   }
-
 
   const logOut = () => {
 
@@ -106,32 +109,21 @@ const App = () => {
 
   }
 
+
+
+
   if (user === null) {
     return (
       <div>
         <h1>login into application</h1>
-        <Notification message={notificationMessage} style={notificationStyle}/>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
+        <Notification message={notificationMessage} style={notificationStyle} />
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          handleUsername={({ target }) => setUsername(target.value)}
+          handlePassword={({ target }) => setPassword(target.value)}
+        />
       </div>
     )
   }
@@ -139,43 +131,24 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+
       <p>{user.username} logged in <button onClick={logOut}>logout</button></p>
-
       <h2>create new</h2>
-      <Notification message={notificationMessage} style={notificationStyle}/>
+      <Notification message={notificationMessage} style={notificationStyle} />
 
-      <form onSubmit={createNewBlog}>
+      <Togglable buttonLabel={'add new blog'} ref={addFormRef}>
         <div>
-          title:
-            <input
-            type="text"
-            value={title}
-            name="Title"
-            onChange={({ target }) => setTitle(target.value)}
+          <AddForm
+            handleSubmit={createNewBlog}
+            title={title}
+            author={author}
+            url={url}
+            handleTitleChange={({ target }) => setTitle(target.value)}
+            handleAuthorChange={({ target }) => setAuthor(target.value)}
+            handleUrlChange={({ target }) => setUrl(target.value)}
           />
         </div>
-        <div>
-          author:
-            <input
-            type="text"
-            value={author}
-            name="Author"
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url:
-            <input
-            type="text"
-            value={url}
-            name="Url"
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-
-
-      </form>
+      </Togglable>
 
       {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
 
